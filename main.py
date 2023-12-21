0f64468677f2b4748f70b949f4e49d7012cde595
Alexsandr <artamoshkinas@gmail.com>
2023-12-21 20:47:17 +0700

    1

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from db import Database
-from test import isvalid_email
+from test import isvalid_email, add_cockies
 
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, redirect, make_response, url_for
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] ='5a2414632370aaca4f6dec257eccc6602d1531399983871c35cd48b30a6ae899'
+app.config['SECRET_KEY'] = '5a2414632370aaca4f6dec257eccc6602d1531399983871c35cd48b30a6ae899'
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -14,13 +15,21 @@ def login():
                         name='varchar(100)',
                         email='varchar(40)',
                         password='text')
-        if isvalid_email(request.form.get('email'),db.query_email('User',request.form.get('email'))):
+        if isvalid_email(request.form.get('email'), db.query_email('User', request.form.get('email'))):
             db.insert_data('User', {'name': request.form.get('name'),
-                                'email': request.form.get('email'),
-                                'password': request.form.get('psw')})
-            flash('Пользователь создан')
+                                    'email': request.form.get('email'),
+                                    'password': request.form.get('psw')})
+            return redirect(url_for('welcome_page')), request.form.get('name')
     return render_template('login.html')
 
+@app.route("/", methods=["GET"])
+def index():
+    response = make_response(login())
+    response.set_cookie('username',  max_age=3600 * 30 * 24)
+    return response
+@app.route('/welcome_page', methods=['GET', 'POST'])
+def welcome_page():
+    return render_template('welcome_page.html', username=request.cookies.get('username'))
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,6 +1,20 @@
-def isvalid_email(first_email: str, second_email: str)-> bool:
+from flask import make_response
+
+
+def isvalid_email(first_email: str, second_email: str) -> bool:
     """Проверка зарегестрированных пользователей по emails"""
     if first_email == second_email:
         return False
     else:
         return True
+
+
+def add_cockies(value: str):
+    res = make_response('welcome_page')
+    res.set_cookie('username', value, max_age=30 * 24 * 3600)
+    return res
+
+def del_coockies():
+    response = make_response('')
+    response.set.coockie('username','', 0)
+    return response
